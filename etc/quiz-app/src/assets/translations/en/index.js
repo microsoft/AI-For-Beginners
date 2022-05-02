f2595c43787a05654860596943f78ca3feb6fa9d
Jen Looper <jen.looper@gmail.com>
2022-05-02 13:44:21 -0400

    quizzes and edits to ch 7

--- etc/quiz-app/src/assets/translations/en/index.js
+++ etc/quiz-app/src/assets/translations/en/index.js
@@ -3,5 +3,7 @@ import x2 from "./lesson-2.json";
 import x3 from "./lesson-3.json";
 import x4 from "./lesson-4.json";
 import x5 from "./lesson-5.json";
-const quiz = { 0 : x1[0], 1 : x2[0], 2 : x3[0], 3 : x4[0], 4 : x5[0] }; 
+import x7 from "./lesson-7.json";
+import x8 from "./lesson-8.json";
+const quiz = { 0 : x1[0], 1 : x2[0], 2 : x3[0], 3 : x4[0], 4 : x5[0], 5 : x7[0], 6 : x8[0] }; 
 export default quiz;
