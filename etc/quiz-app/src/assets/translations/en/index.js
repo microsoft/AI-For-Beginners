73074172d4228218a224c3d6641ebf634c1f93cd
Jen Looper <jen.looper@gmail.com>
2022-05-02 16:12:35 -0400

    quiz23

--- etc/quiz-app/src/assets/translations/en/index.js
+++ etc/quiz-app/src/assets/translations/en/index.js
@@ -5,5 +5,6 @@ import x4 from "./lesson-4.json";
 import x5 from "./lesson-5.json";
 import x7 from "./lesson-7.json";
 import x8 from "./lesson-8.json";
-const quiz = { 0 : x1[0], 1 : x2[0], 2 : x3[0], 3 : x4[0], 4 : x5[0], 5 : x7[0], 6 : x8[0] }; 
+import x23 from "./lesson-23.json";
+const quiz = { 0 : x1[0], 1 : x2[0], 2 : x3[0], 3 : x4[0], 4 : x5[0], 5 : x7[0], 6 : x8[0], 7 : x23[0] }; 
 export default quiz;
