7059027feed42ac70a4685712ea6cc631b0b36f6
semercim <semercim@gmail.com>
2022-09-25 21:12:50 +0200

    Finished Turkish translation of lesson 3's quiz.

--- etc/quiz-app/src/assets/translations/tr/index.js
+++ etc/quiz-app/src/assets/translations/tr/index.js
@@ -1,9 +1,11 @@
 import tr1 from './lesson-1.json';
 import tr2 from './lesson-2.json';
+import tr3 from './lesson-3.json';
 //add items here
 const quiz = {
   0: tr1[0],
-  1: tr2[0]
+  1: tr2[0],
+  2: tr3[0]
 };
 
 export default quiz;
