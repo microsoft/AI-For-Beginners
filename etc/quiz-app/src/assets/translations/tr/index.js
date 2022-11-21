75384bf5f0ca9ef4dbcf11bb3617b5aecadaa827
semercim <semercim@gmail.com>
2022-11-21 17:43:54 +0100

    Translation of lesson 6 to Turkish.

--- etc/quiz-app/src/assets/translations/tr/index.js
+++ etc/quiz-app/src/assets/translations/tr/index.js
@@ -3,13 +3,15 @@ import tr2 from './lesson-2.json';
 import tr3 from './lesson-3.json';
 import tr4 from './lesson-4.json';
 import tr5 from './lesson-5.json';
+import tr6 from './lesson-6.json';
 //add items here
 const quiz = {
   0: tr1[0],
   1: tr2[0],
   2: tr3[0],
   3: tr4[0],
-  4: tr5[0]
+  4: tr5[0],
+  5: tr6[0]
 };
 
 export default quiz;
