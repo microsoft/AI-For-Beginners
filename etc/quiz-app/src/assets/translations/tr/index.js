5cff8d678693aa0a010f4cf31061a16f54ef1f2f
semercim <semercim@gmail.com>
2022-11-27 23:29:37 +0100

    Translation of lesson 7 to Turkish.

--- etc/quiz-app/src/assets/translations/tr/index.js
+++ etc/quiz-app/src/assets/translations/tr/index.js
@@ -4,6 +4,7 @@ import tr3 from './lesson-3.json';
 import tr4 from './lesson-4.json';
 import tr5 from './lesson-5.json';
 import tr6 from './lesson-6.json';
+import tr7 from './lesson-7.json';
 //add items here
 const quiz = {
   0: tr1[0],
@@ -11,7 +12,8 @@ const quiz = {
   2: tr3[0],
   3: tr4[0],
   4: tr5[0],
-  5: tr6[0]
+  5: tr6[0],
+  6: tr7[0]
 };
 
 export default quiz;
