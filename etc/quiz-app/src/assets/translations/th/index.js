cd0244b231f8deff2804e47a7acf7bd3952c0896
Pawopiiieee <49211836+Pawopiiieee@users.noreply.github.com>
2023-06-17 14:47:19 +0200

    Added Thai Translation to Quiz for Lesson 3-4

--- etc/quiz-app/src/assets/translations/th/index.js
+++ etc/quiz-app/src/assets/translations/th/index.js
@@ -1,10 +1,14 @@
 import th1 from "./lesson-1.json";
 import th2 from "./lesson-2.json";
+import th3 from "./lesson-3.json";
+import th4 from "./lesson-4.json";
 
 //add items here
 const quiz = {
   0: th1[0],
-  1: th2[0]
+  1: th2[0],
+  2: th3[0],
+  3: th4[0]
 };
 
 export default quiz;
